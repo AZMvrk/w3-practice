1b29a2ccc23f3c116e81b03488bf8264fe6ba0cc
Zoltan_Mark_Antal <zotyaa95@gmail.com>
2023-10-12 10:39:42 +0200

    foreach map filter

--- script.js
+++ script.js
@@ -1,89 +1,54 @@
-function getFirstChars (str, n){
-  let result = ""
-
-  result = str.substring(0, n + 1)
-
-  return result
-
-}
-
-console.log(getFirstChars("codecool", 5))
+const arr = ["kutya", "cica", "mérési hiba"]
+const numbers = [10, 20, 30, 40, 50]
 
 
-let text = "Lorem ipsum dolor et ipsum Ipsum ipsum"
 
-console.log(text.substring(6, 11))
+arr.forEach(function (element){
+  console.log(element)
+})
+
+arr.forEach((element) => console.log(element))
 
 
-const obj = {
-firstName: "Richard",
-lastName: "Martha",
-getFullName: function(){
-  return obj.firstName + obj.lastName
+const newNumbers = []
+
+numbers.forEach(function(number, index, array){
+  let newNumber = number * 2
+  console.log(newNumber, index, array)
+  newNumbers.push(newNumber)
+})
+
+console.log(newNumbers)
+
+
+const newNumbers2 = []
+
+const mapResult = numbers.map(function(number, index, array){
+  let newNumber = number * 2
+  console.log(newNumber, index, array)
+  newNumbers2.push(newNumber)
+  return newNumber
+})
+
+
+//const mapResult = numbers.map((number) => number * 2)
+
+console.log(newNumbers2)
+console.log(mapResult)
+
+
+const numberArray = [2, 31, 4, 6, 17, 5, 91]
+/* const filterResult = numberArray.filter(function(number){
+  if (number % 2 === 0){
+    return number
   }
-}
+})
 
-console.log(obj.getFullName())
-
-console.log(text.indexOf("Ipsum"))
-
-console.log(text.split(" "))
-
-//console.log(text.join())
-
-let newText = text.replaceAll("ipsum", "kismacska")
-console.log(newText)
-
-//console.log(text.replace("lorem", "kismacska"))
-
-console.log(newText.repeat(3).toLowerCase())
-
-console.log(text.includes("Lorem"))
-
-const randomNumber = Math.round(Math.random()*(100-1)+1) 
-console.log(randomNumber)
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-
-const numberInString = "12,7"
-
-const number = parseInt(numberInString)
-const float = Number(numberInString)
-
-let arr = ["egy", "kettö", "három",]
-arr.push("négy", "öt", "hat",)
-
-const spliceResult = arr.splice(3, 2, "spliceItem1", "sliceItem2")
-
-console.log(spliceResult)
-
-const firstElement = arr.shift()
-const lastElement = arr.pop()
-
-let reverseArr = arr.reverse()
-
-console.log(arr)
-console.log(firstElement)
-console.log(lastElement)
+console.log(filterResult)
+ */
 
 
+const filterResult = numberArray.filter((number)  => number % 2 === 0)
 
-let numbersArr = [1, 22, 333, 4, 54, 76, 1001]
-console.log(numbersArr.sort()) //abc
-console.log(numbersArr.sort((a, b) => a-b)) //növekvő
-console.log(numbersArr.sort((a, b) => b-a)) //csökkenő
-
-let objBeer = {
-  name: "Márta Richárd",
-  age: 28,
-  favoriteDrink: "beer"
-
-}
-
-for(const key of Object.keys(objBeer)){
-  console.log(key)
-  console.log(objBeer[key])
-}
-
-
-console.log(Object.entries(objBeer))
-console.log(Object.keys(objBeer))
+  
+console.log(filterResult)
